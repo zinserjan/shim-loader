51812b8cf6795a3e3869f9640c55f3a066fdf259
Jan-André Zinser <zinserjan@gmail.com>
2016-08-10 22:34:40 +0200

    fixing invalid require statements
    - fixes problems with libraries that contain requires that cannot be
      resolved but include proper export statements

--- src/transform/bindWindowWithoutExports.js
+++ src/transform/bindWindowWithoutExports.js
@@ -2,10 +2,12 @@ import { shimRequire } from './constant';
 
 
 export default function bindWindowWithoutExports() {
+  // if a module doesn't need anything to be exported, it is likely, that it exports itself properly
+  // therefore it is not a good idea to override the module here, however we need to still disable require,
 
   return {
-    header: `\n(function webpackShim(define) {\n\n`,
-    footer: `\n\n}).call(global, undefined);\n`,
+    header: `\n(function webpackShim(module, define, require) {\n\n`,
+    footer: `\n\n}).call(global, module, undefined, undefined);\n`,
   }
 
 }
